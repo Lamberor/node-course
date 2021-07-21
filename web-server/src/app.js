db9d4eede00989a348249c2cda10d80795df676b
cedriclai <cedriclai@tencent.com>
2021-07-21 14:24:31 +0800

    finish the weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -33,16 +35,52 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'This is some helpful text.',
+    helpText: 'Any feedback is welcome!',
     title: 'Help',
     name: 'Cedric Lai',
   });
 });
 
 app.get('/weather', (req, res) => {
+  const address = req.query.address;
+
+  if (!address) {
+    return res.send({
+      error: 'You must provide an address!',
+    });
+  }
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address,
+      });
+    });
+  });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term!',
+    });
+  }
+
+  console.log(req.query.search);
   res.send({
-    forecast: 'It is snowing',
-    location: 'Philadelphia',
+    products: [],
   });
 });
 
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,33 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=c219439e62419e1ad13795257768931f&query=' +
+    latitude +
+    ',' +
+    longitude +
+    '&units=f';
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find loaction!', undefined);
+    } else {
+      const temperature = body.current.temperature;
+      const feelslike = body.current.feelslike;
+      const weather_descriptions = body.current.weather_descriptions;
+      callback(
+        undefined,
+        weather_descriptions +
+          '. It is curently ' +
+          temperature +
+          ' degrees out. ' +
+          'It feels like ' +
+          feelslike +
+          ' degrees out.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
