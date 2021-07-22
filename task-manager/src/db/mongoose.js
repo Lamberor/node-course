156eca0fd684de16e2ecc4d640e11d5a985c59f8
cedriclai <cedriclai@tencent.com>
2021-07-22 15:24:39 +0800

    implemnet basic CRUD operations in MongoDB

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
+  },
+});
+
+const me = new User({
+  name: 'Mike',
+  email: 'mike@',
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log('Error!', error);
+  });
+
+// const Task = mongoose.model('Task', {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// const task = new Task({
+//   description: 'Pass the interview',
+//   completed: false,
+// });
+
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((error) => {
+//     console.log('Error!', error);
+//   });
